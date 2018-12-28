7353a671fa6b00950c5a664bc4c0ed5813673f7a
HeL <aku@jouningakure.com>
2018-12-28 10:40:32 +0700

    add react-cms demo

---
+++ react-cms/precache-manifest.99ee045d8e67c4f0751319db26eb8116.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-cms/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "51c5f7e16af625f93c01",
+    "url": "/react-cms/static/css/main.c59731ba.chunk.css"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/react-cms/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "f7eb2b1914ff6dbc0bcb",
+    "url": "/react-cms/static/js/1.f7eb2b19.chunk.js"
+  },
+  {
+    "revision": "0f4d9fbb45fe814fce70",
+    "url": "/react-cms/static/js/runtime~main.0f4d9fbb.js"
+  },
+  {
+    "revision": "51c5f7e16af625f93c01",
+    "url": "/react-cms/static/js/main.51c5f7e1.chunk.js"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/react-cms/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/react-cms/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/react-cms/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "f7eb2b1914ff6dbc0bcb",
+    "url": "/react-cms/static/css/1.a20950e0.chunk.css"
+  },
+  {
+    "revision": "9f1eac19c10dbd0efd21c0c5711939f9",
+    "url": "/react-cms/index.html"
+  }
+];
---
+++ react-cms/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-cms/precache-manifest.99ee045d8e67c4f0751319db26eb8116.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-cms/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
