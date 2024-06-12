ace1821e172bda1311f61ee1983d2d0b7e3abae8
HeL <aku@jouningkaure.com>
2024-06-12 23:44:49 +0700

    re-initiate git log

---
+++ react-cms/precache-manifest.24fca58c5eb090260d9ec33e81b373eb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-cms/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8038ff9615a9e34ac7a2",
+    "url": "/react-cms/static/css/main.b18235c4.chunk.css"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/react-cms/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "3282a4ceee6b7e735c1d",
+    "url": "/react-cms/static/js/1.3282a4ce.chunk.js"
+  },
+  {
+    "revision": "0f4d9fbb45fe814fce70",
+    "url": "/react-cms/static/js/runtime~main.0f4d9fbb.js"
+  },
+  {
+    "revision": "8038ff9615a9e34ac7a2",
+    "url": "/react-cms/static/js/main.8038ff96.chunk.js"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/react-cms/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/react-cms/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/react-cms/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "3282a4ceee6b7e735c1d",
+    "url": "/react-cms/static/css/1.2c679442.chunk.css"
+  },
+  {
+    "revision": "35197189a59f08019dc734be5ef26ea2",
+    "url": "/react-cms/index.html"
+  }
+];
